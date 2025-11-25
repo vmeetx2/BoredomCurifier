6d77c6172846f108d75c724390ed87fd85aa2317
Veer Meet <vmeet.research@gmail.com>
2025-11-25 18:12:46 +0530

    Create staticarray.java

---
+++ staticarray.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class staticarray{
+    public static void main(String[]args){
+    ArrayList<String> dynamicArray = new ArrayList<>();
+    dynamicArray.add("a");
+    dynamicArray.add("b");
+    dynamicArray.add("c");
+    System.out.println(dynamicArray);
+    System.out.println("empty:" + dynamicArray.size());
+    System.out.println("empty:" + dynamicArray.isEmpty());
+
+    }
+}
